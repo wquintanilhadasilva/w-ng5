8ba9596e4092f8d8c988d291e3f6b1582c7a9d8d
Juliano <juliano.brasil@gmail.com>
2018-10-27 10:49:16 -0300

    feat: ignore portuguese special chars (#1)
    - @angular general upgrade

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,10 +1,11 @@
-import { Pipe, PipeTransform, NgModule } from '@angular/core';
+// tslint:disable-next-line:no-implicit-dependencies
+import { NgModule } from '@angular/core';
 
 import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [FilterPipe],
   exports: [FilterPipe],
-  providers: [FilterPipe]
+  providers: [FilterPipe],
 })
-export class PipesModule { }
+export class PipesModule {}
